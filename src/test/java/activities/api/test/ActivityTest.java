cf3bd729dff4dc893d1db9a7d771aefd9e8c77d6
Oleksii Bezborodov <oleksii.bezborodov@techfabric.com>
2024-01-27 17:18:39 +0100

    Refactoring tests pre setup

--- src/test/java/activities/api/test/ActivityTest.java
+++ src/test/java/activities/api/test/ActivityTest.java
@@ -42,9 +42,7 @@ public class ActivityTest {
                     .build();
 
             try (var response = client.newCall(request).execute()) {
-                if (!response.isSuccessful()) {
-                    Assert.fail("Response is not successful, with code " + response.code() + " and message " + response.message());
-                }
+                Assert.assertTrue(response.isSuccessful(),"Response is not successful, with code " + response.code() + " and message " + response.message());
                 var bodyString = response.body().string();
                 var activities = new JSONArray(bodyString);
                 Assert.assertEquals(activities.length(), 30);
@@ -63,35 +61,10 @@ public class ActivityTest {
                     .build();
 
             try (var response = client.newCall(request).execute()) {
-                if (!response.isSuccessful()) {
-                    Assert.fail("Response is not successful, with code " + response.code() + " and message " + response.message());
-                }
+                Assert.assertTrue(response.isSuccessful(),"Response is not successful, with code " + response.code() + " and message " + response.message());
                 var bodyString = response.body().string();
                 var activity = new JSONObject(bodyString);
                 Assert.assertEquals(activity.get("title"), "Activity 1");
-            }
-        } catch (IOException ex) {
-            Assert.fail(ex.getMessage());
-        }
-    }
-
-    @Test
-    public void putActivityShouldAcceptData() {
-        try {
-            var updateActivity = new Activity(100500, "Activity x", "2021-07-01T00:00:00+00:00", true);
-            var requestBody = mapper.writeValueAsString(updateActivity);
-            var request = new Request.Builder()
-                    .url(baseUrl + "/1")
-                    .put(RequestBody.create(requestBody, MediaType.parse("application/json")))
-                    .build();
-
-            try (var response = client.newCall(request).execute()) {
-                if (!response.isSuccessful()) {
-                    Assert.fail("Response is not successful, with code " + response.code() + " and message " + response.message());
-                }
-                var bodyString = response.body().string();
-                var updatedActivity = mapper.readValue(bodyString, Activity.class);
-                Assert.assertEquals(updateActivity, updatedActivity);
             }
         } catch (IOException ex) {
             Assert.fail(ex.getMessage());
@@ -110,9 +83,7 @@ public class ActivityTest {
                     .build();
 
             try (var response = client.newCall(request).execute()) {
-                if (!response.isSuccessful()) {
-                    Assert.fail("Response is not successful, with code " + response.code() + " and message " + response.message());
-                }
+                Assert.assertTrue(response.isSuccessful(),"Response is not successful, with code " + response.code() + " and message " + response.message());
                 var bodyString = response.body().string();
                 var createdActivity = mapper.readValue(bodyString, Activity.class);
                 Assert.assertEquals(newActivity, createdActivity);
@@ -123,18 +94,63 @@ public class ActivityTest {
     }
 
     @Test
+    public void putActivityShouldAcceptData() {
+        try {
+
+            var newActivity = new Activity(100500, "Activity x", "2021-07-01T00:00:00+00:00", true);
+            var requestBodyJson = mapper.writeValueAsString(newActivity);
+
+            var postRequest = new Request.Builder()
+                    .url(baseUrl)
+                    .post(RequestBody.create(requestBodyJson, MediaType.parse("application/json")))
+                    .build();
+
+            try (var postResponse = client.newCall(postRequest).execute()) {
+                Assert.assertTrue(postResponse.isSuccessful(),"Response is not successful, with code " + postResponse.code() + " and message " + postResponse.message());
+            }
+
+            var updateActivity = new Activity(100500, "Activity x", "2021-07-01T00:00:00+00:00", true);
+            var requestBody = mapper.writeValueAsString(updateActivity);
+            var request = new Request.Builder()
+                    .url(baseUrl + "/100500")
+                    .put(RequestBody.create(requestBody, MediaType.parse("application/json")))
+                    .build();
+
+            try (var response = client.newCall(request).execute()) {
+                Assert.assertTrue(response.isSuccessful(),"Response is not successful, with code " + response.code() + " and message " + response.message());
+                var bodyString = response.body().string();
+                var updatedActivity = mapper.readValue(bodyString, Activity.class);
+                Assert.assertEquals(updateActivity, updatedActivity);
+            }
+        } catch (IOException ex) {
+            Assert.fail(ex.getMessage());
+        }
+    }
+
+
+    @Test
     public void deleteActivityShouldBeSuccessfulData() {
         try {
+
+            var newActivity = new Activity(1, "Activity x", "2021-07-01T00:00:00+00:00", true);
+            var requestBodyJson = mapper.writeValueAsString(newActivity);
+
+            var postRequest = new Request.Builder()
+                    .url(baseUrl)
+                    .post(RequestBody.create(requestBodyJson, MediaType.parse("application/json")))
+                    .build();
+
+            try (var postResponse = client.newCall(postRequest).execute()) {
+                Assert.assertTrue(postResponse.isSuccessful(),"Response is not successful, with code " + postResponse.code() + " and message " + postResponse.message());
+            }
+
             var request = new Request.Builder()
                     .url(baseUrl + "/1")
                     .delete()
                     .build();
 
             try (var response = client.newCall(request).execute()) {
-                if (!response.isSuccessful()) {
-                    Assert.fail("Response is not successful, with code " + response.code() + " and message " + response.message());
-                }
-                Assert.assertTrue(response.isSuccessful());
+                Assert.assertTrue(response.isSuccessful(),"Response is not successful, with code " + response.code() + " and message " + response.message());
             }
         } catch (IOException ex) {
             Assert.fail(ex.getMessage());
